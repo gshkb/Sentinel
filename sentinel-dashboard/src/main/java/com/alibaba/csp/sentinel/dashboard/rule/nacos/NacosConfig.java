6a48afb8a16a2f2040046927fbe47d1e1a1b34ae
hekb <w99699w@126.com>
2020-04-09 18:10:44 +0800

    整合配置

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
@@ -18,32 +18,44 @@ package com.alibaba.csp.sentinel.dashboard.rule.nacos;
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
 import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.nacos.api.PropertyKeyConst;
 import com.alibaba.nacos.api.config.ConfigFactory;
 import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author Eric Zhao
  * @since 1.4.0
  */
-//@Configuration
+@Configuration
 public class NacosConfig {
 
-    //@Bean
+    @Value("${nacos.address}")
+    private String address;
+    @Value("${nacos.namespace}")
+    private String namespace;
+
+    @Bean
     public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
         return JSON::toJSONString;
     }
 
-    //@Bean
+    @Bean
     public Converter<String, List<FlowRuleEntity>> flowRuleEntityDecoder() {
         return s -> JSON.parseArray(s, FlowRuleEntity.class);
     }
 
-    //@Bean
+    @Bean
     public ConfigService nacosConfigService() throws Exception {
-        return ConfigFactory.createConfigService("localhost");
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, address);
+        properties.put(PropertyKeyConst.NAMESPACE, namespace);
+        return ConfigFactory.createConfigService(properties);
+       // return ConfigFactory.createConfigService(address);
     }
 }
